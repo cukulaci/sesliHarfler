38cafb90bf0c704bd1f8f356d92a8e267fc67d5f
Emre SAGLAM <69041945+cukulaci@users.noreply.github.com>
2023-07-14 23:57:09 +0300

    Add files via upload

---
+++ sesliHarfler/src/sesliHarfler/Main.java
@@ -0,0 +1,24 @@
+package sesliHarfler;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char harf = 'A';
+		
+		switch(harf) {
+		case 'A':
+		case 'E':
+		case 'I':
+		case 'O':
+			System.out.println("Sesli harf");
+			break;
+			
+		default:
+			System.out.println("Sessiz harf");
+		}
+
+	}
+
+}
